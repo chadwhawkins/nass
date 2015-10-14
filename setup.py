15944e7ca280c0c9128554f1e1f148616c9947b8
Nick Frost <nickfrostatx@gmail.com>
2015-10-14 13:36:17 -0700

    Officially support pypy

--- setup.py
+++ setup.py
@@ -34,6 +34,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
     extras_require={
         'testing': ['pytest>=2.0.0'],
